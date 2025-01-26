ffabd77dcfec86081511e2313c52de3eca7c9d7a
MEHTHAB7 <mehthab225@gmail.com>
2025-01-26 13:50:25 +0530

    Task completed

--- src/components/DeleteEmoloyee.jsx
+++ src/components/DeleteEmoloyee.jsx
@@ -5,6 +5,7 @@ const DeleteEmployee = () => {
     return (
         <div>
             <EmployeeNavbar />
+            <h2 align="center">DELETE EMPLOYEE</h2>
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
